22ae2a5d6f05bb1fb7928a57c4492376b894f0b3
Arunabh Das <arunabhdas@gmail.com>
2022-11-10 15:20:51 -0500

    Added products call to home route

--- codekit/src/routes/+page.svelte
+++ codekit/src/routes/+page.svelte
@@ -1,3 +1,14 @@
 <h1>Welcome to CodeKit</h1>
 <p>The Meta Platform for Building Platforms</p>
-<a href="/about">About CodeKit</a>
+
+<script>
+    export let data;
+    const { products } = data;
+</script>
+{#each products as product}
+    <div>
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+    </div>
+{/each}
