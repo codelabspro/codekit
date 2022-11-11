f9af5a8503b13c8ab97dd0079592a29d7b356b63
Arunabh Das <arunabhdas@gmail.com>
2022-11-11 03:10:18 -0500

    Added news page

--- codekit/src/routes/news/+page.server.js
+++ codekit/src/routes/news/+page.server.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 export const load = async () => {
     const fetchMovies = async () => {
-        const fetchMovies = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`);
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`);
         const data = await res.json()
         return data.results
     }
---
+++ codekit/src/routes/news/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data
+    const { movies } = data
+
+</script>
+
+<h1>News</h1>
+{#each movies as movie}
+    <div>
+        <h2>{movie.title}</h2>
+        <p>{movie.overview}</p>
+    </div>
+{/each}
