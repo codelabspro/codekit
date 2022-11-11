a455f130a3359a839f0358fbac574a0611046ccd
Arunabh Das <arunabhdas@gmail.com>
2022-11-11 03:45:20 -0500

    Updated tailwind.config.cjs for daisyui

--- codekit/tailwind.config.cjs
+++ codekit/tailwind.config.cjs
@@ -5,7 +5,19 @@ const config = {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require("daisyui")],
+
+	  // daisyUI config (optional)
+	  daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	  },
 };
 
 module.exports = config;
